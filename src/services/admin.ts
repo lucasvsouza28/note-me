60695c2a800fd488f37e568fdd01f927083fb2e9
Lucas Souza <lucasvsouza28@gmail.com>
2022-02-24 21:46:49 -0300

    wip: removing secrets dependency

--- src/services/admin.ts
+++ src/services/admin.ts
@@ -1,13 +1,11 @@
 import * as firebaseAdmin from 'firebase-admin';
 
-import serviceAccount from './secret.json';
-
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: process.env.PVK_private_key,
+      clientEmail: process.env.PVK_client_email,
+      projectId: process.env.PVK_project_id,
     }),
     databaseURL: 'https://note-me-e70ba.firebaseio.com',
   });
--- src/services/client.ts
+++ src/services/client.ts
@@ -4,11 +4,11 @@ import 'firebase/compat/auth';
 if (typeof window !== 'undefined' && !firebase.apps.length) {
   firebase.initializeApp({
     apiKey: "AIzaSyADGrQyDjHrmPn4lORMs5ImzwZdHimKDk4",
-  authDomain: "note-me-e70ba.firebaseapp.com",
-  projectId: "note-me-e70ba",
-  storageBucket: "note-me-e70ba.appspot.com",
-  messagingSenderId: "725006324478",
-  appId: "1:725006324478:web:29e04bd81aa86528688baf"
+    authDomain: "note-me-e70ba.firebaseapp.com",
+    projectId: "note-me-e70ba",
+    storageBucket: "note-me-e70ba.appspot.com",
+    messagingSenderId: "725006324478",
+    appId: "1:725006324478:web:29e04bd81aa86528688baf"
   });
   firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 }
